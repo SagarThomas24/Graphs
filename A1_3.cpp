ab750cae814de442a5590ba45aaf617d60478733
SagarThomas24 <156944561+SagarThomas24@users.noreply.github.com>
2025-03-21 13:40:32 +0530

    Update Tomita.cpp

--- A1_3.cpp
+++ A1_3.cpp
@@ -1,89 +1,121 @@
 #include <iostream>
 #include <unordered_map>
-#include <set>
+#include <unordered_set>
 #include <fstream>
 #include <vector>
-#pragma GCC optimize("O3,unroll-loops,fast-math,tree-vectorize,omit-frame-pointer,inline-functions")
+#include <algorithm>
+#include <map>
 
-int c=1;
+#pragma GCC optimize("O3")
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+
 using namespace std;
 
-void expand(set<int> Q, set<int> CAND, set<int> FINI, const unordered_map<int, set<int>>& graph) 
-{
-    if (CAND.empty() && FINI.empty()) 
-    {
-        cout << "{ ";
-        for (auto i : Q) {
-            cout << i << " ";
+map<int, int> sizeCounts;
+unordered_map<int, unordered_set<int>> graph;
+unordered_set<int> Q;
+int n, m;
+int c = 1;
+int k=200000;
+
+int findPivot(const unordered_set<int>& CAND) {
+    int pivot = -1, max_intersection = -1;
+    for (int u : CAND) {
+        int intersection_count = 0;
+        for (int neighbor : graph.at(u)) {
+            if (CAND.count(neighbor)) {  
+                intersection_count++;
+            }
         }
-        cout << "}\n";
-        cout<<c<<endl;
+        if (intersection_count > max_intersection) {  
+            max_intersection = intersection_count;
+            pivot = u;
+        }
+    }
+    return pivot;  
+}
+
+void expand(unordered_set<int> subg, unordered_set<int> CAND) {
+    if (subg.empty()) {
+        sizeCounts[Q.size()]++;
+        if(c==k)
+        {
+            cout<<c<<endl;
+            k=k+200000;
+        }
         c++;
         return;
-
     }
-    
-    set<int> CAND_copy = CAND;
-    
-    for (auto q : CAND_copy) 
-    {
-        set<int> Q_new = Q;
-        Q_new.insert(q);
-        
-        set<int> CAND_new, FINI_new;
-        for (auto v : CAND) {
-            if (graph.at(q).count(v)) {
-                CAND_new.insert(v);
-            }
+
+    int pivot = findPivot(CAND);
+    unordered_set<int> EXT;
+    for (int v : CAND) {
+        if (!graph[pivot].count(v)) {
+            EXT.insert(v);
         }
-        for (auto v : FINI) {
-            if (graph.at(q).count(v)) {
-                FINI_new.insert(v);
+    }
+
+    while (!EXT.empty()) {
+        int q = *EXT.begin();
+        EXT.erase(q);
+
+        Q.insert(q);
+        unordered_set<int> newSUBG, newCAND;
+
+        for (int neighbor : graph[q]) {
+            if (subg.count(neighbor)) {
+                newSUBG.insert(neighbor);
+            }
+            if (CAND.count(neighbor)) {
+                newCAND.insert(neighbor);
             }
         }
 
-        expand(Q_new, CAND_new, FINI_new, graph);
-
+        expand(newSUBG, newCAND);
+        Q.erase(q);
         CAND.erase(q);
-        FINI.insert(q);
     }
 }
 
-void findMaximalCliques(int n, const unordered_map<int, set<int>>& graph) 
-{
-    set<int> Q, CAND, FINI;
-    for (const auto& node : graph) 
-    {
-        CAND.insert(node.first);
+void findMaximalCliques() {
+    unordered_set<int> V;
+    for (auto& node : graph) {
+        V.insert(node.first);
     }
-    expand(Q, CAND, FINI, graph);
+    expand(V, V);
 }
 
-int main()
-{
-    ifstream inputFile("input.txt"); 
-    if (!inputFile) 
-    {
+int main() {
+    ifstream inputFile("inpute.txt");
+    if (!inputFile) {
         cerr << "Error: Could not open input.txt" << endl;
         return 1;
     }
 
-    int n, m;
-    inputFile >> n >> m;  
+    cout << "Graph Loaded" << endl;
+    cout << "Tomita Research Paper Implementation" << endl;
 
-    unordered_map<int, set<int>> graph;  
+    inputFile >> n >> m;
+    cout << "Vertices: " << n << " , Edges: " << m << endl;
 
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         int u, v;
         inputFile >> u >> v;
         graph[u].insert(v);
         graph[v].insert(u);
     }
 
-    inputFile.close();  
+    inputFile.close();
+    findMaximalCliques();
 
-    findMaximalCliques(n, graph);  
+    cout << "Total Cliques: " << c - 1 << endl;
+    cout << "Size of Cliques: " << endl;
+
+    for (auto it = sizeCounts.begin(); it != sizeCounts.end(); it++) {
+        cout << it->first << " : " << it->second << endl;
+    }
 
     return 0;
 }
